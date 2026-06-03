82408de9044512a376f6e0c5d0812676a202e304
Kavya Srivastava <skavya12669@gamil.com>
2026-06-03 09:19:42 +0530

    Initial commit

---
+++ auth.js
@@ -0,0 +1,4 @@
+console.log("hello");
+   let shivani=require("./auth.js")
+console.log("bye");
+console.log(shivani);
---
+++ main.js
@@ -0,0 +1,7 @@
+let login=true;
+let anshika="hello";
+let auth=()=>{
+    console.log("authentication success");
+}
+console.log(login);
+module.exports={login,anshika};
